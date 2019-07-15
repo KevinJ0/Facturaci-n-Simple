c88c05795771f541a0bdea0708d73165d637e654
KevinJ0 <kevinjooo59@gmail.com>
2019-07-15 10:58:17 -0400

    Tamaño del font de las tablas reparado

--- miBD_inventario_clientes.sql
+++ miBD_inventario_clientes.sql
@@ -2,10 +2,10 @@
 
 create table cliente( cedula varchar(13), nombre varchar(50) not null,
  direccion varchar(70), telefono varchar(20),id integer auto_increment,primary key (id));
+ drop table inventario;
+create table inventario(id integer auto_increment, articulo varchar(50), cantidad int unsigned,
+ precio float,fecha date, primary key (id));
  
-create table inventario( articulo varchar(50), cantidad int,
- precio float,fecha date,id integer auto_increment, primary key (id));
- 
-select* from cliente;
+select* from inventario;
   
   
